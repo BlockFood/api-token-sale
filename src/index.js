7000b6f2ec38e12248773c4464bddea552f15577
Conrad <conrad@blockfood.io>
2017-12-17 15:37:21 +0100

    basic api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,52 @@
+const express = require('express')
+const multiparty = require('multiparty')
+
+const getApp = (handlers = {
+    add: () => {},
+    update: () => {},
+    get: async () => {},
+}) => {
+    const app = express()
+
+    app.get('/pre-sale/new', (req, res) => {
+        handlers.add(req.query.email)
+        res.end()
+    })
+
+    app.post('/pre-sale/edit/:privateId', (req, res) => {
+        const form = new multiparty.Form({ autoFiles: true })
+
+        form.parse(req, (err, fields, files) => {
+            if (err) {
+                res.send(500)
+                return
+            }
+            const privateId = req.params.privateId
+            const application = Object.keys(fields).reduce((application, key) => {
+                application[key] = fields[key][0]
+                return application
+            }, {})
+            const idCardPath = files['id_card'][0].path
+
+            handlers.update(privateId, application, idCardPath)
+            res.end()
+        })
+    })
+
+    app.get('/pre-sale/review/:privateId', async (req, res) => {
+        const application = await handlers.get(req.params.privateId)
+        res.send(application)
+    })
+
+    return app
+}
+
+const start = (app, port) => {
+    console.log('BlockFood-API :: start on port ', port)
+    app.listen(port)
+}
 
 module.exports = {
-    start: () => {}
+    getApp,
+    start
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,99 @@
-const { expect } = require('chai')
+const { expect, assert } = require('chai')
+const sinon = require('sinon')
+const supertest = require('supertest')
 
-const index = require('./index')
+const fs = require('fs')
+
+const { start, getApp } = require('./index')
 
 describe('index', () => {
-    it('should be defined', () => {
-        expect(index).not.to.be.null
+
+    describe('start', () => {
+        it('shoud call listen on app', () => {
+            const app = {
+                listen: sinon.stub()
+            }
+
+            start(app, 1337)
+
+            assert(app.listen.calledWith(1337))
+        })
+    })
+
+    describe('getApp', () => {
+        it('should return an express app', () => {
+            const app = getApp()
+
+            expect(app).not.to.be.null
+            expect(app.listen).not.to.be.null
+        })
+
+        describe('GET /pre-sale/new', () => {
+            it('should accept new applications', async () => {
+                const app = getApp()
+
+                await supertest(app)
+                    .get('/pre-sale/new?email=test@foo.bar')
+                    .expect(200)
+            })
+            it('should call handlers.add', async () => {
+                const handler = {
+                    add: sinon.stub()
+                }
+                const app = getApp(handler)
+
+                await supertest(app)
+                    .get('/pre-sale/new?email=test@foo.bar')
+                    .expect(200)
+
+                assert(handler.add.calledWith('test@foo.bar'))
+            })
+        })
+
+        describe('POST /pre-sale/edit/:privateId', () => {
+            it('should call handlers.update with valid parameters', async() => {
+                const handler = {
+                    update: sinon.stub()
+                }
+                const expectedPreSaleApplication = {
+                    name : 'what'
+                }
+                const app = getApp(handler)
+
+                await supertest(app)
+                    .post('/pre-sale/edit/ID42')
+                    .field('name', 'what')
+                    .attach('id_card', 'test/fixture/logo.png')
+                    .expect(200)
+
+                const [ firstCall ] = handler.update.getCalls()
+
+                const [ id, application, idCardPath ] = firstCall.args
+                expect(id).to.equal('ID42')
+                expect(application).to.deep.equal(expectedPreSaleApplication)
+
+                assert(fs.existsSync(idCardPath))
+            })
+
+        })
+
+        describe('GET /pre-sale/review/:privateId', () => {
+            it('should return whatever handlers.get returns', async() => {
+                const handler = {
+                    get: sinon.stub()
+                }
+                const whatever = { whatevs : true}
+                handler.get.withArgs('ID42').resolves(whatever)
+
+                const app = getApp(handler)
+
+                const response = await supertest(app)
+                    .get('/pre-sale/review/ID42')
+                    .expect(200)
+
+                expect(response.body).to.deep.equal(whatever)
+            })
+        })
+
     })
 })
