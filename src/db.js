0750f8386409fd80db00f8f4e583613547c08c51
Conrad <conrad@blockfood.io>
2017-12-18 10:26:27 +0100

    add db handler

---
+++ src/db.js
@@ -0,0 +1,30 @@
+const MongoClient = require('mongodb').MongoClient
+
+module.exports = async (dbUri, collectionName = 'pre-sale-applications') => {
+    const client = await MongoClient.connect(dbUri)
+    const db = client.db('token-sale')
+    const collection = db.collection(collectionName)
+
+    return {
+        add: async (application) => {
+            await collection.insertOne(application)
+        },
+
+        update: async (privateId, application) => {
+            await collection.findOneAndUpdate(
+                { privateId },
+                { $set: application }
+            )
+        },
+
+        get: async (privateId) => {
+            return await collection.findOne({ privateId })
+        },
+
+        getWithPublicId: async (publicId) => {
+            return await collection.findOne({ publicId })
+        },
+
+        close: async () => client.close()
+    }
+}
---
+++ src/db.test.js
@@ -0,0 +1,73 @@
+const { expect, assert } = require('chai')
+const sinon = require('sinon')
+
+const MongoInMemory = require('mongo-in-memory')
+
+const db = require('./db')
+
+const mongoServerInstance = new MongoInMemory(25625)
+
+const getDbUri = async () => new Promise((resolve, reject) => {
+    mongoServerInstance.start((error, config) =>
+        error ? reject(error) : resolve(mongoServerInstance.getMongouri('whatevs'))
+    )
+})
+
+const stopDbInstance = async () => new Promise((resolve, reject) =>
+    mongoServerInstance.stop(error => error ? reject(error) : resolve())
+)
+
+describe('db', () => {
+    let dbUri, dbHandler
+    before(async () => {
+        dbUri = await getDbUri()
+    })
+    after(async () => {
+        stopDbInstance()
+    })
+    beforeEach(async() => {
+        dbHandler = await db(dbUri)
+    })
+    afterEach(async() => {
+        dbHandler && dbHandler.close()
+    })
+
+    describe('add', () => {
+        it('should add the application in the db', async() => {
+            await dbHandler.add({ email: 'foo@bar', privateId: 'privateId', publicId: 'publicId' })
+
+            const application = await dbHandler.get('privateId')
+
+            expect(application.email).to.equal('foo@bar')
+            expect(application.privateId).to.equal('privateId')
+            expect(application.publicId).to.equal('publicId')
+        })
+    })
+
+    describe('update', () => {
+        it('should update the application in the db', async() => {
+            await dbHandler.add({ email: 'foo@bar', privateId: 'privateId', publicId: 'publicId' })
+
+            await dbHandler.update('privateId', { newField: true})
+
+            const application = await dbHandler.get('privateId')
+
+            expect(application.email).to.equal('foo@bar')
+            expect(application.privateId).to.equal('privateId')
+            expect(application.publicId).to.equal('publicId')
+            expect(application.newField).to.equal(true)
+        })
+    })
+
+    describe('getWithPublicId', () => {
+        it('returns the application with the given publicId', async() => {
+            await dbHandler.add({ email: 'foo@bar', privateId: 'privateId', publicId: 'publicId' })
+
+            const application = await dbHandler.getWithPublicId('publicId')
+
+            expect(application.email).to.equal('foo@bar')
+            expect(application.privateId).to.equal('privateId')
+            expect(application.publicId).to.equal('publicId')
+        })
+    })
+})
