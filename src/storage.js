258a2dc995de32074d038e9b3c080bfa992d2c69
Conrad <conrad@blockfood.io>
2017-12-18 09:29:41 +0100

    add storage handler

---
+++ src/storage.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+const fs = require('fs-extra-promise')
+const randomID = require('random-id')
+
+
+module.exports = (storagePath) => {
+
+    return {
+        store: async (filePath) => {
+            const extension = path.extname(filePath)
+            const newFileName = randomID(15) + extension
+
+            await fs.copyAsync(filePath, path.join(storagePath, newFileName))
+
+            return newFileName
+        }
+    }
+}
