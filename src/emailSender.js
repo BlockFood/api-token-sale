5850805a4c4dc9abeac35f5858227e81cb6c0f35
Conrad <conrad@blockfood.io>
2017-12-18 11:03:50 +0100

    add html escape

--- src/emailSender.js
+++ src/emailSender.js
@@ -1,3 +1,5 @@
+const escape = require('escape-html')
+
 module.exports = (send, getNextStepUrl) => {
 
     return {
@@ -14,11 +16,12 @@ module.exports = (send, getNextStepUrl) => {
         sendSecondEmail: async (email, application) => {
             await send({
                 title: 'BlockFood - One final step for pre-sale',
-                content: `<p>Dear ${application.firstName}, thank you for submitting your documents.</p>
+                content: `<p>Dear ${escape(application.firstName)}, thank you for submitting your documents.</p>
                     <p>If you did not already use the smart contract to finalize your application, follow this link:</p>
                     <p><a href='${getNextStepUrl(application.privateId)}' class='call-to-action'>Finalize application</a></p>
                     <p>Once your application is finalized, the BlockFood team will review your information and accept your participation if everything is okay. You should receive a confirmation email in the next 24 hours.</p>
                     <p>Thanks again for being awesome.</p>`,
+                
 
                 signature: 'Best regards,<br><br>BlockFood team'
             })
--- src/emailSender.test.js
+++ src/emailSender.test.js
@@ -3,10 +3,9 @@ const sinon = require('sinon')
 
 const emailSender = require('./emailSender')
 
-
 describe('emailSender', () => {
     describe('sendFirstEmail', () => {
-        it('calls send with some content', async() => {
+        it('calls send with some content', async () => {
             const send = sinon.stub()
             const getNextStepUrl = privateId => `url/to/pre-sale-form/${privateId}`
 
@@ -25,18 +24,20 @@ describe('emailSender', () => {
         })
     })
 
-    describe('sendSecondEmail', () => async() => {
-        it('calls send with some content', async() => {
+    describe('sendSecondEmail', () => {
+        it('calls send with some content', async () => {
+            console.log('???')
             const send = sinon.stub()
             const getNextStepUrl = privateId => `url/to/pre-sale-form/${privateId}`
 
             const application = {
-                email : 'foo@bar',
-                firstName : 'foo',
-                lastName : 'bar'
+                email: 'foo@bar',
+                firstName: 'foo<p>bar</p>',
+                lastName: 'bar',
+                privateId: 'privateId'
             }
 
-            await emailSender(send, getNextStepUrl).sendSecondEmail('foo@bar', 'privateId')
+            await emailSender(send, getNextStepUrl).sendSecondEmail('foo@bar', application)
 
             assert(send.calledOnce)
 
@@ -45,7 +46,7 @@ describe('emailSender', () => {
 
             expect(sent.title).to.equal('BlockFood - One final step for pre-sale')
             expect(sent.signature).to.equal('Best regards,<br><br>BlockFood team')
-            expect(sent.content).to.equal(`<p>Dear foo, thank you for submitting your documents.</p>
+            expect(sent.content).to.equal(`<p>Dear foo&lt;p&gt;bar&lt;/p&gt;, thank you for submitting your documents.</p>
                     <p>If you did not already use the smart contract to finalize your application, follow this link:</p>
                     <p><a href='url/to/pre-sale-form/privateId' class='call-to-action'>Finalize application</a></p>
                     <p>Once your application is finalized, the BlockFood team will review your information and accept your participation if everything is okay. You should receive a confirmation email in the next 24 hours.</p>
